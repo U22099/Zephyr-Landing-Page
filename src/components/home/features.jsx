b849e2b448fbf381f3aff74368fa495929cd64d0
U22099 <nifemiolaniyi4@gmail.com>
2024-12-08 14:56:07 +0100

    update

--- src/components/home/features.jsx
+++ src/components/home/features.jsx
@@ -1,7 +1,42 @@
+import {
+  Card,
+  CardContent
+} from "@/components/ui/card";
+import { motion } from "framer-motion";
 
 export function Features(){
+  const features = [
+    {header: "Personal Chats", content: "Seamless one-on-one chats with text, images, videos, and audio", image: "z-chats.jpg"},
+    {header: "Group Chat", content: "Create vibrant group chats and share the fun with friends", image: "z-newchats.jpg"},
+    {header: "Video/Voice Calls", content: "Stay connected face-to-face or via voice calls", image: "z-call2.jpg"},
+    {header: "Status Updates", content: "Share memories and status post anywhere anytime", image: "z-status.jpg"},
+    {header: "AI Chatbot and Customization", content: "Smart AI chatbot and fully customizable interface", image: "z-ai.jpg"},
+  ]
   return(
-    <main>
+    <main className="w-full gap-1 flex flex-col">
+      <motion.h1 initial = {{opacity: 0}} whileInView = {{opacity: 1}} transition = {{duration: 0.3}} className="text-2xl font-bold text-black dark:text-white border-b-2 border-b-primary pb-1 mb-2">What Makes Zephyr Awesome?</motion.h1>
+      {features.map(feature => <FeatureCard content={feature.text} header={feature.header} image={feature.image}/>)}
     </main>
+  )
+}
+
+function FeatureCard({ header, content, image }){
+  return(
+    <motion.div initial = {{opacity: 0, y: 100}} whileInView = {{opacity: 1, y: 0}} transition = {{duration: 0.3}} classm="w-full">
+      <Card className="p-2 w-full">
+        <CardContent className="flex flex-col">
+          <div className="h-40 w-40 relative rounded">
+            <Image
+              src={`/images/${image}`}
+              objectFit="cover"
+              layout="fill"
+              alt={header}
+            />
+          </div>
+          <h1 className="text-xl font-bold text-black dark:text-white border-b-2 border-b-primary pb-1">{header}</h1>
+          <p className="text-muted-foreground">{content}</p>
+        </CardContent>
+      </Card>
+    </motion.div>
   )
 }
--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 
 export function Hero(){
   return(
-    <main className="flex flex-col justify-center items-center text-center w-full h-60">
+    <main className="flex flex-col w-full h-40 justify-center items-center text-center relative">
       <Image
-        src="/images/z-chats.jpg"
+        src="/images/z-smillingman.jpg"
         objectFit="cover"
         layout="fill"
         alt="Zephyr Chat"
         className="z-[-10]"
       />
-      <h1 className="text-3xl font-extrabold text-primary">Zephyr: Your All-in-One Chat App</h1>
-      <h3 className="text-xl text-muted font-bold">Connect, chat, and create with seamless communication</h3>
+      <div className="flex flex-col justify-center items-center text-center w-fit p-4 rounded backdrop-blur-xl border">
+        <h1 className="text-3xl font-extrabold text-primary">Zephyr: Your All-in-One Chat App</h1>
+        <h3 className="text-xl text-muted-foreground font-bold">Connect, chat, and create with seamless communication</h3>
+      </div>
     </main>
   )
 }
