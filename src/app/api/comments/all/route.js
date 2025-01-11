2f347104e45c6c47fa65e0f5964240017962aa26
U22099 <nifemiolaniyi4@gmail.com>
2025-01-11 22:17:30 +0100

    update route.js

--- src/app/api/comments/all/route.js
+++ src/app/api/comments/all/route.js
@@ -8,6 +8,7 @@ export const GET = async () => {
     await connectToDb();
     const results = await Comment.find()
     .sort({ timestamp: -1 })
+    .limit(10000)
     .lean();
     return NextResponse.json({ data: results}, { status: 200 });
   } catch (err) {
